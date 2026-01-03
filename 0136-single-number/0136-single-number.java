14c8fe369c0b78c927194a1d6787108c27597c8e
Vaibhavi Garg <vaibhavigarg86@gmail.com>
2026-01-03 08:06:57 +0530

    Time: 1 ms (99.87%), Space: 47 MB (28.02%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       
+        int res = 0;
+
+        for (int n : nums) {
+            res ^= n;
+        }
+
+        return res;        
+    }
+}
+
+
+   
